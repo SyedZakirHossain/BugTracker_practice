61db1c4ed8f3ee108747c18902d9d67a2b1a361f
Zakir <inspirayhan@gmail.com>
2023-03-15 11:12:47 +0600

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,4 @@
+from bugapp.models import Team,Contact,Customer
+
+Contact.objects.create(name='ff',email='ff@ff.io',desc='this is an issue')
+Customer.objects.create(name='ff',email='ff@ff.io',desc='this is an issue')
