76c8606632a1aa40aefeb7397cbb67eeaddeb6c4
Zakir <inspirayhan@gmail.com>
2023-03-26 01:12:54 +0600

    Delete ex.py

--- ex.py
+++
@@ -1,4 +0,0 @@
-from bugapp.models import Team,Contact,Customer
-
-Contact.objects.create(name='ff',email='ff@ff.io',desc='this is an issue')
-Customer.objects.create(name='ff',email='ff@ff.io',desc='this is an issue')
