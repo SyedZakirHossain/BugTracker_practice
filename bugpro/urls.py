ceedb9108ddb7b85be82f6022abb026d2e819d9a
Zakir <inspirayhan@gmail.com>
2023-03-26 01:00:25 +0600

    Update urls.py

--- bugpro/urls.py
+++ bugpro/urls.py
@@ -1,12 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bugapp.urls')),
 ]
-
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)   
 
 admin.site.index_title ="The Issue Tracker"
 admin.site.site_header ="The Issue Tracker Admin"
